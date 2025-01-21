0c1f8b808597972f3a3d30a23ad8be4d49d95c7c
Mummoth <GluetrapGames@gmail.com>
2025-01-21 17:08:30 +0000

    Fixed  Naming

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float m_MoveSpeed = 5.0f;
-    private Controlls _controlls;
+    private Controls _controls;
 
     private void Update()
     {
-        var moveINput = _controlls.Player.Move.ReadValue<Vector2>();
+        var moveINput = _controls.Player.Move.ReadValue<Vector2>();
         var movementVec = new Vector3(moveINput.x, moveINput.y, 0.0f) * (m_MoveSpeed * Time.deltaTime);
 
 
@@ -18,13 +18,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnEnable()
     {
-        _controlls = new Controlls();
-        _controlls.Player.Enable();
+        _controls = new Controls();
+        _controls.Player.Enable();
     }
 
     private void OnDisable()
     {
-        _controlls.Player.Disable();
-        _controlls = null;
+        _controls.Player.Disable();
+        _controls = null;
     }
 }
