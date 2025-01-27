f1d17999c38090daf534485ebcaae65a93704595
Mummoth <GluetrapGames@gmail.com>
2025-01-27 19:15:15 +0000

    Created a Chroma Key Shader.
    I created a custom shader for chroma keying and replaced the areas with a pattern.

--- Assets/_Root/Scripts/Switcher.cs
+++ Assets/_Root/Scripts/Switcher.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Switcher : MonoBehaviour
 {
 	public KeyCode m_SwitchKey = KeyCode.Alpha0;
+	public bool m_SwitchComponent;
 	public List<GameObject> m_SwitchObjects = new();
 	public List<TextMeshProUGUI> m_SwitchObjectUis = new();
 
@@ -13,7 +15,11 @@ public class Switcher : MonoBehaviour
 		if (!Input.GetKeyDown(m_SwitchKey)) return;
 
 		foreach (var t in m_SwitchObjects)
-			t.gameObject.SetActive(!t.activeSelf);
+			if (m_SwitchComponent)
+				t.GetComponent<ShadowCaster2D>().enabled =
+					!t.GetComponent<ShadowCaster2D>().enabled;
+			else
+				t.gameObject.SetActive(!t.activeSelf);
 
 		foreach (var element in m_SwitchObjectUis)
 			element.gameObject.SetActive(!element.gameObject.activeSelf);
