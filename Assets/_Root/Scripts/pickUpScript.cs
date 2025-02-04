3f59afa7651974827fe7cd8a19f719288f7fb24b
gianhel <GluetrapGames@gmail.com>
2025-02-04 15:39:09 +0000

    Multiple Changes (Desc)
    Added test scene
    Added P&C movement to test scene
    Added pickup prefab + pickup logics

--- Assets/_Root/Scripts/pickUpScript.cs
+++ Assets/_Root/Scripts/pickUpScript.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pickUpScript : MonoBehaviour
 {
     public bool isClicked;
+    public string itemType;
+    public InventorySlot[] itemSlots;
+    public GameObject itemPrefab;
+    public Sprite sprite;
+    private bool _slotFound = false;
 
     // Start is called before the first frame update
     void Start()
@@ -34,12 +40,38 @@ public class pickUpScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        while (!_slotFound)
+        {
+            Debug.Log("slotFound is false");
+            for (int i = 0; i < itemSlots.Length; i++)
+            {
+                if (itemSlots[i].transform.childCount == 0 && _slotFound == false)
+                {
+                    Debug.Log("Slot" + itemSlots[i].name + " is empty");
+                    itemPrefab.GetComponent<InventoryItem>().itemType = itemType;
+                    itemPrefab.GetComponent<Image>().sprite = sprite;
+                    GameObject.Instantiate(itemPrefab, itemSlots[i].transform);
+                    _slotFound = true;
+                    Debug.Log("Added " + itemType + " to slot " + itemSlots[i].name + " - Type Validation: " + itemPrefab.GetComponent<InventoryItem>().itemType);
+                }
+                
+                if (i == itemSlots.Length - 1 && _slotFound == false)
+                {
+                    Debug.LogWarning("Inventory full while attempting " + itemType +" spawning, exiting while loop");
+                    _slotFound = true;
+                }
+            }
+        }
+        
+        _slotFound = false;
+        
         // If the game object has been clicked and the player is within the trigger then hide the object and display alert message.
         if (isClicked) 
         { 
-            //Debug.Log("Item collected");
+            Debug.Log("Item collected");
             PixelCrushers.DialogueSystem.DialogueManager.ShowAlert(gameObject.name + " has been collected!");
             gameObject.SetActive(false); 
         }
+        
     }
 }
