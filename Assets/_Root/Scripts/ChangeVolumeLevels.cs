8ec6aa598d983f645e20cdd3cc45e302bf383f77
gianhel <GluetrapGames@gmail.com>
2025-02-11 15:16:40 +0000

    Breakable Items

--- Assets/_Root/Scripts/ChangeVolumeLevels.cs
+++ Assets/_Root/Scripts/ChangeVolumeLevels.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -26,4 +26,4 @@ public class ChangeVolumeLevels : MonoBehaviour
             AkSoundEngine.SetRTPCValue("vol_music", musicVol);
         }
     }
-}
+}*/
