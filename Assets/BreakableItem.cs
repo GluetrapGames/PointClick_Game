c7fcdff9ad0ccf785368f04abffe6459052e6c16
Mummoth <piwonipiotr@gmail.com>
2025-02-11 15:41:54 +0000

    Commit Cleanup

--- Assets/BreakableItem.cs
+++ Assets/BreakableItem.cs
@@ -1,40 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BreakableItem : MonoBehaviour
 {
+	public static InventoryItem PlayerHeldItem;
 
-    public int itemHp;
-    public CollideCheck itemCollision;
-    
-    public static InventoryItem PlayerHeldItem;
-    private string _heldItemType = PlayerHeldItem.itemType;
-    public string effectiveItemType;
+	public int itemHp;
+	public CollideCheck itemCollision;
+	//private string _heldItemType = PlayerHeldItem.itemType;
+	public string effectiveItemType;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && itemCollision.IsCollided)
-        {
-            Debug.Log("Damage Called");
-            Damage();
-        }else if(Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("Damage failed to call, no collision detected");
-        }
-    }
-    
-    private void Damage()
-    {
-        itemHp = itemHp - 1;
-        Debug.Log(transform.name + " took 1 damage - New HP = " + itemHp);
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E) && itemCollision.IsCollided)
+		{
+			Debug.Log("Damage Called");
+			Damage();
+		}
+		else if (Input.GetKeyDown(KeyCode.E))
+			Debug.Log("Damage failed to call, no collision detected");
+	}
 
-        if (itemHp <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-        
-    }
+	private void Damage()
+	{
+		itemHp = itemHp - 1;
+		Debug.Log(transform.name + " took 1 damage - New HP = " + itemHp);
 
+		if (itemHp <= 0) Destroy(gameObject);
+	}
 }
