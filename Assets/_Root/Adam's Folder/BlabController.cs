d4d68e8698118bdf8ea33c5d5f72090c34e5e44e
GluetrapGames <GluetrapGames@gmail.com>
2025-02-20 17:00:23 +0000

    Working Blabs
    Blabs are working. Randomized pitch change at the start of each loop to give the speaker some character. Also pauses when the history log is open.

--- Assets/_Root/Adam's Folder/BlabController.cs
+++ Assets/_Root/Adam's Folder/BlabController.cs
@@ -1,64 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using PixelCrushers.DialogueSystem;
 using System;
 using AYellowpaper.SerializedCollections;
 using UnityEngine.Rendering;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BlabController : MonoBehaviour
 {
     [SerializeField]
     private AYellowpaper.SerializedCollections.SerializedDictionary<String, AudioClip> _ClipDictionary;
-    private string speakerName;
-
+    [SerializeField]
+    private AudioSource _AudioSource;
+    [SerializeField]
+    private GameObject _LogWindow;
+    private float _LastAudioTime = 0.0f;
+    private string _SpeakerName;
     public UnityUITypewriterEffect m_TypeWritterEffect;
+
+    // Update checks if an audio clip is currently playing.
+    // Then changes the pitch for the next clip loop.
+    private void Update()
+    {
+        if (_AudioSource.isPlaying)
+        {
+            // Detect when the clip loops
+            if (_AudioSource.time < _LastAudioTime) // Loop detected
+            {
+                _AudioSource.pitch = Random.Range(0.8f, 1.2f);
+            }
+            _LastAudioTime = _AudioSource.time;
+        }
+
+        // If the hitory window is open, then pause the blabs.
+        if (_LogWindow.activeInHierarchy)
+            _AudioSource.Pause();
+        else
+            _AudioSource.UnPause();
+    }
 
     private void OnConversationStart(Transform subtitle)
     {
-        //Debug.Log(DialogueManager.CurrentConversationState.subtitle.listenerInfo.Name);
-        //string conversationTitle = DialogueManager.lastConversationStarted;
-        //if (!string.IsNullOrEmpty(conversationTitle))
-        //{
-        //    var conversation = DialogueManager.masterDatabase.GetConversation(conversationTitle);
-        //    if (conversation.dialogueEntries.Count > 0)
-        //    {
-        //        var firstEntry = conversation.dialogueEntries[0];
-        //        var speakerInfo = DialogueManager.MasterDatabase.GetActor(firstEntry.ActorID);
-
-        //        if (speakerInfo != null)
-        //        {
-        //            Debug.Log("First Speaker Name: " + speakerInfo.Name);
-        //        }
-        //    }
-        //}
     }
 
-
+    // When a conversation line begins, gets the current speaker's name
+    // And calls for audio to play.
     void OnConversationLine(Subtitle subtitle)
     {
-        speakerName = subtitle.speakerInfo.Name;
+        _SpeakerName = subtitle.speakerInfo.Name;
         PlayActorClip();
-        Debug.Log(speakerName);
+        Debug.Log(_SpeakerName);
     }
 
-
+    // Plays the audio clip associated with the speaker from the database.
     public void PlayActorClip()
     {
-        Debug.Log("Entered audio");
+        //Debug.Log("Entered audio");
         foreach (var actor in _ClipDictionary)
         {
-            if (speakerName == actor.Key)
+            if (_SpeakerName == actor.Key)
             {
                 m_TypeWritterEffect.audioClip = actor.Value;
             }
         }
     }
 
-    public void StopActorClip() 
+    // Stops the audio clip at the end of the conversation line.
+    public void StopActorClip()
     {
         m_TypeWritterEffect.audioClip = null;
-        m_TypeWritterEffect.audioSource.Stop();
+        _AudioSource.Stop();
+        _LastAudioTime = 0f;
     }
-
 }
+
