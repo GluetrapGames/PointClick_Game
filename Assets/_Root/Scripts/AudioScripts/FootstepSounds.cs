cf7451225ed9bf6dfdc0b1c4117a935e2e57c0fd
Lewis Docherty <57623215+wiisd@users.noreply.github.com>
2025-02-25 17:40:26 +0000

    More footstep stuff
    still not working properly im losing it

--- Assets/_Root/Scripts/AudioScripts/FootstepSounds.cs
+++ Assets/_Root/Scripts/AudioScripts/FootstepSounds.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FootstepSounds : MonoBehaviour
@@ -7,39 +8,61 @@ public class FootstepSounds : MonoBehaviour
     public string whatMaterial;
     public float footstepSpeed;
     private bool playingFootsteps;
-    private PlayerGridController PlayerGridController;
+    [SerializeField]
+    private GameObject Player;
+    private GridMovement PlayerGridController;
 
     /// <summary>
     /// Idk why this isnt working, Adam found a workaround for now.
     /// </summary>
+    private void Awake()
+    {
+        PlayerGridController = Player.GetComponent<GridMovement>();
+    }
 
-    //public void switchFootMat()
-    //{
-    //    // Default to wood
-    //    if (whatMaterial != null) { AkSoundEngine.SetSwitch("FootstepMaterial", "Wood", gameObject); }
 
-    //    // Set switch in Wwise 
-    //    AkSoundEngine.SetSwitch("FootstepMaterial", whatMaterial, gameObject);
+    public void switchFootMat()
+    {
+        // Default to wood
+        if (whatMaterial != null) { AkSoundEngine.SetSwitch("FootstepMaterial", "Wood", gameObject); }
 
-    //}
+        // Set switch in Wwise 
+        AkSoundEngine.SetSwitch("FootstepMaterial", whatMaterial, gameObject);
 
-    //private void Update()
-    //{
-    //    if (PlayerGridController._movement.m_IsMoving) { startFootsteps(); }
-    //    else { stopFootsteps(); }
-    //}
+    }
 
-    //public void startFootsteps()
-    //{
-    //    playingFootsteps = true;
-    //    InvokeRepeating("postFootstep", 0f, footstepSpeed);
-    //}
+    private void Update() 
+    {
+        if (PlayerGridController.m_IsMoving)
+        {
+            if (!playingFootsteps)
+            {
+                startFootsteps();
+            }
 
-    //private void stopFootsteps()
-    //{
-    //    playingFootsteps = false;
-    //    CancelInvoke(nameof(postFootstep));
-    //}
+        if (PlayerGridController.m_IsMoving == false)
+            {
+                if (playingFootsteps)
+                {
+                    stopFootsteps();
+                }
+            }
+        }
+    }
+
+    public void startFootsteps()
+    {
+        playingFootsteps = true;
+        InvokeRepeating("postFootstep", 0f, footstepSpeed);
+        Debug.Log("Started Footstep");
+    }
+
+    private void stopFootsteps()
+    {
+        playingFootsteps = false;
+        CancelInvoke("postFootstep");
+        Debug.Log("Stopped Footstep");
+    }
 
     // This gets called every 3 frames on the albert animation for now
     private void postFootstep()
