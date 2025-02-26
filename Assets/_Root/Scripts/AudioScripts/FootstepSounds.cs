99a9920429cb6b38031a0b606a51a4d9eabb1b8e
Mummoth <piwonipiotr@gmail.com>
2025-02-26 13:47:22 +0000

    Fixed Footsteps Audio Bug

--- Assets/_Root/Scripts/AudioScripts/FootstepSounds.cs
+++ Assets/_Root/Scripts/AudioScripts/FootstepSounds.cs
@@ -1,71 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FootstepSounds : MonoBehaviour
 {
-    public string whatMaterial;
-    public float footstepSpeed;
-    private bool playingFootsteps;
-    [SerializeField]
-    private GameObject Player;
-    private GridMovement PlayerGridController;
+	public string whatMaterial;
+	public float footstepSpeed;
+	[SerializeField]
+	private GameObject Player;
+	private GridMovement PlayerGridController;
+	private bool playingFootsteps;
 
-    /// <summary>
-    /// Idk why this isnt working, Adam found a workaround for now.
-    /// </summary>
-    private void Awake()
-    {
-        PlayerGridController = Player.GetComponent<GridMovement>();
-    }
+	/// <summary>
+	///     Idk why this isnt working, Adam found a workaround for now.
+	/// </summary>
+	private void Awake()
+	{
+		PlayerGridController = Player.GetComponent<GridMovement>();
+	}
+
+	private void Update()
+	{
+		if (PlayerGridController.m_IsMoving)
+		{
+			if (!playingFootsteps) startFootsteps();
+		}
+		else
+		{
+			if (playingFootsteps) stopFootsteps();
+		}
+	}
 
 
-    public void switchFootMat()
-    {
-        // Default to wood
-        if (whatMaterial != null) { AkSoundEngine.SetSwitch("FootstepMaterial", "Wood", gameObject); }
+	public void switchFootMat()
+	{
+		// Default to wood
+		if (whatMaterial != null)
+			AkSoundEngine.SetSwitch("FootstepMaterial", "Wood", gameObject);
 
-        // Set switch in Wwise 
-        AkSoundEngine.SetSwitch("FootstepMaterial", whatMaterial, gameObject);
+		// Set switch in Wwise 
+		AkSoundEngine.SetSwitch("FootstepMaterial", whatMaterial, gameObject);
+	}
 
-    }
+	public void startFootsteps()
+	{
+		playingFootsteps = true;
+		InvokeRepeating("postFootstep", 0f, footstepSpeed);
+		Debug.Log("Started Footstep");
+	}
 
-    private void Update() 
-    {
-        if (PlayerGridController.m_IsMoving)
-        {
-            if (!playingFootsteps)
-            {
-                startFootsteps();
-            }
+	private void stopFootsteps()
+	{
+		playingFootsteps = false;
+		CancelInvoke("postFootstep");
+		Debug.Log("Stopped Footstep");
+	}
 
-        if (PlayerGridController.m_IsMoving == false)
-            {
-                if (playingFootsteps)
-                {
-                    stopFootsteps();
-                }
-            }
-        }
-    }
-
-    public void startFootsteps()
-    {
-        playingFootsteps = true;
-        InvokeRepeating("postFootstep", 0f, footstepSpeed);
-        Debug.Log("Started Footstep");
-    }
-
-    private void stopFootsteps()
-    {
-        playingFootsteps = false;
-        CancelInvoke("postFootstep");
-        Debug.Log("Stopped Footstep");
-    }
-
-    private void postFootstep()
-    {
-        AkSoundEngine.PostEvent("Footstep", gameObject);
-    }
+	private void postFootstep()
+	{
+		AkSoundEngine.PostEvent("Footstep", gameObject);
+	}
 }
