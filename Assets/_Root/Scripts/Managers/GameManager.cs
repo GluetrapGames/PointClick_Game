581906316911283e2ec642b71e4e24381eda1d1d
Mummoth <piwonipiotr@gmail.com>
2025-03-11 16:50:14 +0000

    Fixed DialogueV3 Branch Merge Issues

--- Assets/_Root/Scripts/Managers/GameManager.cs
+++ Assets/_Root/Scripts/Managers/GameManager.cs
@@ -27,6 +27,7 @@ public class GameManager : Singleton<GameManager>
 	private States _PreviousState;
 
 	public InventoryManager m_InventoryManager { get; private set; }
+	public EndGameTracker m_EndGameTracker { get; private set; }
 	public PlayerGridController m_Player { get; private set; }
 	public Grid m_Grid { get; private set; }
 	public Tilemap m_NavMesh { get; private set; }
@@ -40,6 +41,7 @@ public class GameManager : Singleton<GameManager>
 	{
 		base.Awake();
 		m_InventoryManager = FindFirstObjectByType<InventoryManager>();
+		m_EndGameTracker = FindFirstObjectByType<EndGameTracker>();
 		InitGame();
 		_PreviousState = _CurrentState;
 	}
@@ -177,12 +179,11 @@ public class GameManager : Singleton<GameManager>
 	}
 }
 
-	public enum States
-	{
-		Moving = 0,
-		Talking = 1,
-		Interacting = 2,
-		InMenus = 3
-	}
+public enum States
+{
+	Moving = 0,
+	Talking = 1,
+	Interacting = 2,
+	InMenus = 3
 }
 }
