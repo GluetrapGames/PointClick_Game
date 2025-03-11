3e1773d77bc14f2d2c67314738c1563e8ce65fc9
Adam <GluetrapGames@gmail.com>
2025-03-11 13:50:08 +0000

    scenery dialogue  events

--- Assets/_Root/Scripts/Dialogue System Extentions/SetTypewriterSpeedToAudioLength.cs
+++ Assets/_Root/Scripts/Dialogue System Extentions/SetTypewriterSpeedToAudioLength.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 namespace PixelCrushers.DialogueSystem
 {
-
-    /// <summary>
-    /// Add to a subtitle text GameObject to adjust the typewriter speed to match the current
-    /// audio clip.
-    /// </summary>
     public class SetTypewriterSpeedToAudioLength : MonoBehaviour
     {
+
+        public AudioSource audioSource;
+
         void OnTextChange(UITextField textField)
         {
             StartCoroutine(SetTypewriterSpeed(textField.text));
@@ -19,8 +17,8 @@ namespace PixelCrushers.DialogueSystem
         {
             yield return null;
             yield return null;
-            var typewriter = GetComponent<AbstractTypewriterEffect>();
-            var audioSource = DialogueManager.currentConversationState.subtitle.speakerInfo.transform.GetComponent<AudioSource>();
+            var typewriter = GetComponent<TextMeshProTypewriterEffect>();
+            //var audioSource = DialogueManager.currentConversationState.subtitle.speakerInfo.transform.GetComponent<AudioSource>();
             if (typewriter != null && audioSource != null && audioSource.clip != null)
             {
                 typewriter.charactersPerSecond = text.Length / audioSource.clip.length;
