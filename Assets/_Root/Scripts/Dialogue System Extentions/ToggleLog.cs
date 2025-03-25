3f7cf77dda886e0f40b99f873237e92f0e0b4276
Adam <GluetrapGames@gmail.com>
2025-03-25 12:35:15 +0000

    Mark animation and backlog
    Fixed mark's blab animation as he stopped blabbing.
    
    on screen backlog and pause button now hides when the backlog is open.

--- Assets/_Root/Scripts/Dialogue System Extentions/ToggleLog.cs
+++ Assets/_Root/Scripts/Dialogue System Extentions/ToggleLog.cs
@@ -13,7 +13,7 @@ public class ToggleLog : MonoBehaviour
 	[Tooltip("The dialogue system controller script."), SerializeField]
 	private DialogueSystemController _dsController;
 	[Tooltip("The back log example script."), SerializeField]
-	private BackLogExample _blExample;
+	private BackLogExample _blExample;	
 	[Tooltip("The UI continue button"), SerializeField]
 	private Button _ContinueButton;
 	[Tooltip("The UI auto play button"), SerializeField]
@@ -22,9 +22,18 @@ public class ToggleLog : MonoBehaviour
 	private Button _SkipButton;
 	[Tooltip("The response panel"), SerializeField]
 	private GameObject _ResponsePanel;
+	// Image child may be removed
     [Tooltip("The Image child"), SerializeField]
-    private GameObject _ChildImage;
-    [SerializeField,Tooltip("The BACKLOG image ")]
+    private GameObject _ChildImage;    
+	[Tooltip("The Pause button on the screen"), SerializeField]
+    private GameObject _ScreenPauseButton;
+    [Tooltip("The Log button on the screen"), SerializeField]
+    private GameObject _ScreenLogButton;
+
+    
+	// Changes the dialogue panel log button image but this may be removed
+	// in favoour of having the close button on the log panel itself.
+	[SerializeField,Tooltip("The BACKLOG image ")]
 	private Sprite _OpenImage;
     [SerializeField, Tooltip("The CLOSE image")]
     private Sprite _CloseImage;
@@ -33,12 +42,13 @@ public class ToggleLog : MonoBehaviour
 	private bool _IsPlayerListener;
 	private Image _ButtonImage;
 
-	private void Start()
+	private void Awake()
 	{
 		_ButtonImage = _ChildImage.GetComponent<Image>();
 	}
 
-	private void Update()
+
+        private void Update()
 	{
 		// Current fix - If autoplay is active and log panel opens, current subtitle still hides at the end
 		// and the continue button appears. This line should prevent that for now.
@@ -56,12 +66,16 @@ public class ToggleLog : MonoBehaviour
 	{
 		toggleLog = !toggleLog;
 
-		if (DialogueManager.isConversationActive)
+		if (toggleLog) // When the backlog is showing
 		{
-			if (toggleLog) // When the backlog is showing
-			{
-				if (_IsPlayerListener)
-					_ResponsePanel.SetActive(false);
+			if (_ScreenLogButton != null) { _ScreenLogButton.SetActive(false); }
+			if (_ScreenPauseButton != null) { _ScreenPauseButton.SetActive(false); }
+
+			// Only do this if a conversation is open.
+			if (DialogueManager.IsConversationActive) 
+			{ 
+				// Hide the player response panel
+				if (_IsPlayerListener) _ResponsePanel.SetActive(false);
 				// Stops the speed of the current portrait animation.
 				_PortraitAnimator.speed = 0f;
 				// Hide all other buttons
@@ -73,16 +87,24 @@ public class ToggleLog : MonoBehaviour
 				// Turns off autoplay
 				DialogueManager.displaySettings.subtitleSettings.continueButton =
 				DisplaySettings.SubtitleSettings.ContinueButtonMode.Always;
-				// Pauses the pauses the dialogue system
+				// Pauses the dialogue system
 				_dsController.Pause();
-				// Opens and displays the dialogue history
-				_blExample.ShowBackLog();
-				_blExample.OpenLogWindow();
 			}
-			else // When the backlog is hidden
+			
+			// Opens and displays the dialogue history
+			_blExample.ShowBackLog();
+			_blExample.OpenLogWindow();
+		}
+		else // When the backlog is hidden
+		{
+            if (_ScreenLogButton != null) { _ScreenLogButton.SetActive(true); }
+            if (_ScreenPauseButton != null) { _ScreenPauseButton.SetActive(true); }
+
+            // Only do this if a conversation is open.
+            if (DialogueManager.IsConversationActive)
 			{
-				if (_IsPlayerListener)
-					_ResponsePanel.SetActive(true);
+				// Show the player response panel
+				if (_IsPlayerListener) _ResponsePanel.SetActive(true);
 				// Returns the speed of the current portrait animation.
 				_PortraitAnimator.speed = 1.0f; ;
 				// Show all other buttons
@@ -93,9 +115,10 @@ public class ToggleLog : MonoBehaviour
 				_ButtonImage.sprite = _OpenImage;
 				// Unpauses the dialogue system
 				_dsController.Unpause();
-				// Hides the backlog window
-				_LogWindow.SetActive(false);
 			}
+			
+			// Hides the backlog window
+			_LogWindow.SetActive(false);
 		}
 	}
 }
