0a2643d750e4242004159ee683f78ff0d45402e8
Mummoth <piwonipiotr@gmail.com>
2025-03-26 13:37:19 +0000

    Fixed the Phone Sound Issue

--- Assets/_Root/Scripts/AudioScripts/PhoneCallSounds.cs
+++ Assets/_Root/Scripts/AudioScripts/PhoneCallSounds.cs
@@ -1,46 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
 namespace GlueTrap
 {
-    public class PhoneCallSounds : MonoBehaviour
-    {
+public class PhoneCallSounds : MonoBehaviour
+{
+	private bool _PhoneRinging;
 
-        private bool _PhoneRinging = false;
-        void Awake()
-        {
-            if (!_PhoneRinging)
-            {
-                _PhoneRinging = true;
-                AkSoundEngine.PostEvent("PhoneRing", gameObject);
-            }
-           
-        }
+	public void PlayRingSounds()
+	{
+		DialogueEntry dialogueEntry = DialogueManager.currentConversationState
+			.subtitle.dialogueEntry;
+		var conversationID = dialogueEntry.conversationID;
+		Debug.Log(conversationID);
+		if (conversationID == 56)
+			AkSoundEngine.PostEvent("PhoneRing", gameObject);
+		if (conversationID != 59) return;
 
-        public void ahhhh()
-        {
-            var dialogueentry = DialogueManager.currentConversationState.subtitle.dialogueEntry;
-            int conversationID = dialogueentry.conversationID;
-            int subtitleId = dialogueentry.id;
-            if (conversationID == 59 && subtitleId == 1)
-            {
-                AkSoundEngine.PostEvent("StopPhone", gameObject);
-                AkSoundEngine.PostEvent("PhonePickup", gameObject);
-                
-            }
-
-            if (conversationID == 59 && subtitleId == 31)
-            {
-                AkSoundEngine.PostEvent("PhoneHang", gameObject);
-            }
-
-            if (conversationID == 59 && subtitleId == 32)
-            {
-                AkSoundEngine.PostEvent("PhoneSlam", gameObject);
-                AkSoundEngine.PostEvent("StopPhone", gameObject);
-            }
-        }
-    }
+		var subtitleId = dialogueEntry.id;
+		switch (subtitleId)
+		{
+			case 1:
+				AkSoundEngine.PostEvent("StopPhone", gameObject);
+				AkSoundEngine.PostEvent("PhonePickup", gameObject);
+				break;
+			case 31:
+				AkSoundEngine.PostEvent("PhoneHang", gameObject);
+				break;
+			case 32:
+				AkSoundEngine.PostEvent("PhoneSlam", gameObject);
+				AkSoundEngine.PostEvent("StopPhone", gameObject);
+				break;
+		}
+	}
+}
 }
