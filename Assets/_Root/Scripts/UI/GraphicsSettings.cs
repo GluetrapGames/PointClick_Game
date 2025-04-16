0cc309838042332c940715bf31c7ddf147100cd6
Mummoth <piwonipiotr@gmail.com>
2025-04-16 23:20:33 +0100

    Finished the "GraphicsSettings" script
    . Added functionality for automatically populating the quality dropdown with qualities in the project and changing it based on the selected value.
    . Added functionality for automatically populating the resolution dropdown with device-supported resolutions and changing it based on the selected value.
    . Added functionality for having the window full-screen or not.

--- Assets/_Root/Scripts/UI/GraphicsSettings.cs
+++ Assets/_Root/Scripts/UI/GraphicsSettings.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,21 +18,103 @@ public class GraphicsSettings : MonoBehaviour
 
 	private void Start()
 	{
-		// Clear the options in dropdowns if populated.
-		if (_ResolutionDropdown.options.Count > 0)
-			_ResolutionDropdown.ClearOptions();
+		if (_ResolutionDropdown) SetupResolutionDropdown();
+		if (_QualityDropdown) SetupQualityDropdown();
+		if (_FullscreenToggle) _FullscreenToggle.isOn = Screen.fullScreen;
+	}
+
+	private void OnEnable()
+	{
+		// Subscribe to all the OnValueChange events.
+		_QualityDropdown?.onValueChanged.AddListener(delegate
+		{
+			QualityValueChange(_QualityDropdown);
+		});
+		_ResolutionDropdown?.onValueChanged.AddListener(delegate
+		{
+			ResolutionValueChange(_ResolutionDropdown);
+		});
+		_FullscreenToggle?.onValueChanged.AddListener(WindowModeChange);
+	}
+
+	private void OnDisable()
+	{
+		// Unsubscribe from all the OnValueChange events.
+		_QualityDropdown?.onValueChanged.RemoveListener(delegate
+		{
+			QualityValueChange(_QualityDropdown);
+		});
+		_ResolutionDropdown?.onValueChanged.RemoveListener(delegate
+		{
+			ResolutionValueChange(_ResolutionDropdown);
+		});
+		_FullscreenToggle?.onValueChanged.RemoveListener(WindowModeChange);
+	}
+
+	private static void QualityValueChange(TMP_Dropdown qualityDropdown)
+	{
+		// Update quality level.
+		QualitySettings.SetQualityLevel(qualityDropdown.value);
+	}
+
+	private static void ResolutionValueChange(TMP_Dropdown resolutionDropdown)
+	{
+		// Update to the new resolution.
+		Resolution newResolution = Screen.resolutions[resolutionDropdown.value];
+		Screen.SetResolution(newResolution.width, newResolution.height,
+			Screen.fullScreen);
+	}
+
+	private void SetupQualityDropdown()
+	{
+		// Clear the options in dropdown if populated.
 		if (_QualityDropdown.options.Count > 0)
 			_QualityDropdown.ClearOptions();
 
+		// Add all the quality settings into the dropdown.
+		var qualitySettings = QualitySettings.names;
+		_QualityDropdown.AddOptions(qualitySettings.ToList());
 
-		/*var msg = $"Number of Settings: {QualitySettings.count}\nSettings: ";
-		var settings = QualitySettings.names;
-		msg = settings.Aggregate(msg,
-			(current, setting) => current + $"\n{setting}");
-		msg +=
-			$"\nCurrent Setting: {settings[QualitySettings.GetQualityLevel()]}";
+		// Have the current quality value selected.
+		_QualityDropdown.value = QualitySettings.GetQualityLevel();
+	}
 
-		Debug.Log(msg);*/
+	private void SetupResolutionDropdown()
+	{
+		// Clear the options in dropdown if populated.
+		if (_ResolutionDropdown.options.Count > 0)
+			_ResolutionDropdown.ClearOptions();
+
+		// Add all the supported resolutions to the dropdown.
+		var resolutions = Screen.resolutions;
+		var resolutionNames = resolutions
+			.Select(resolution => resolution.ToString()).ToList();
+		_ResolutionDropdown.AddOptions(resolutionNames);
+
+		// Have the current resolution be selected.
+		// Try to find the current resolution in the list.
+		Resolution currentResolution = Screen.currentResolution;
+		var currentIndex = resolutions.ToList().FindIndex(r =>
+			r.width == currentResolution.width &&
+			r.height == currentResolution.height &&
+			Math.Abs(r.refreshRateRatio.value -
+			         currentResolution.refreshRateRatio.value) < 0.0001);
+
+
+		// If failed to find exact resolution, find the next best one.
+		if (currentIndex < 0)
+		{
+			currentIndex = resolutions.ToList().FindIndex(r =>
+				r.width == currentResolution.width &&
+				r.height == currentResolution.height);
+		}
+
+		_ResolutionDropdown.value = currentIndex;
+	}
+
+	private static void WindowModeChange(bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
 	}
 }
 }
