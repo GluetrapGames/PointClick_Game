8552807df1f07895a62ca032eed26c7fba8118b7
Lewis Docherty <GluetrapGames@gmail.com>
2025-04-17 17:25:19 +0100

    Sounds
    Not on the record script, tied to dialogue

--- Assets/_Root/Scripts/AudioScripts/EnvironmentSounds.cs
+++ Assets/_Root/Scripts/AudioScripts/EnvironmentSounds.cs
@@ -231,5 +231,26 @@ public class EnvironmentSounds : MonoBehaviour
             }
         }
 
+        public void PlayRecordSounds()
+        {
+            DialogueEntry dialogueEntry = DialogueManager.currentConversationState
+            .subtitle.dialogueEntry;
+            var conversationID = dialogueEntry.conversationID;
+            var subtitleId = dialogueEntry.id;
+
+            if (conversationID != 34) return;
+
+            switch (subtitleId)
+            {
+                case 3:
+                    AkSoundEngine.PostEvent("MusicRecord", gameObject);
+                    AkSoundEngine.SetRTPCValue("vol_music", 0f);
+                    break;
+                case 5:
+                    AkSoundEngine.PostEvent("StopRecord", gameObject);
+                    AkSoundEngine.SetRTPCValue("vol_music", 5f);
+                    break;
+            }
+        }
         }
 }
