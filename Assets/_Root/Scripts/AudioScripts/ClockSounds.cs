6253360c03239d91f2b633cb411c92d94c95f59a
Lewis Docherty <GluetrapGames@gmail.com>
2025-04-24 14:42:40 +0100

    Removed ui for recording
    -dialogue box remains

--- Assets/_Root/Scripts/AudioScripts/ClockSounds.cs
+++ Assets/_Root/Scripts/AudioScripts/ClockSounds.cs
@@ -14,26 +14,13 @@ namespace GlueTrap
         private bool clockPlaying;
         public bool clockBroken;
         [SerializeField]
-        private bool _Log = true;
+        private bool _Log;
 
-
-        private void Awake()
-        {
-            gameManager = Utils.GetGameManager();
-            ambSound = Utils.GetGameManager().GetComponent<AmbientSound>();
-        }
 
         // Update is called once per frame
         void Update()
         {
-            if (!ambSound.inLivingRoom && clockPlaying)
-            {
-                AkSoundEngine.PostEvent("StopClock", gameObject);
-                clockPlaying = false;
-                if (_Log) { Debug.Log("Clock activated: " + clockPlaying); }
-            }
-
-            if (ambSound.inLivingRoom && !clockPlaying)
+            if (!clockPlaying)
             {
                 clockPlaying = true;
                 AkSoundEngine.PostEvent("CourtClock", gameObject);
